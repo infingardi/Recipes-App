2cf92f2c5e9bed36c590f4598c518b94c54b8fad
Vini_Vergani <vinivergani95@gmail.com>
2022-03-07 17:33:06 -0300

    feat: add forms

---
+++ src/context/LoginProvider.js
@@ -0,0 +1,2 @@
+// import React, { useEffect, useState } from 'react';
+// import LoginContext from './LoginContext';
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
 export default function index() {
+  // const emailContext = useContext;
   return (
-    <div>login</div>
+    <div>
+      <h1>Login</h1>
+      <form>
+
+        <label htmlFor="emailInput">
+          <input
+            type="email"
+            name="emailInput"
+            id="email-input"
+            data-testid="email-input"
+            // value={ email }
+            // onChange={ emailContext }
+          />
+        </label>
+
+        <label htmlFor="passwordInput">
+          <input
+            type="password"
+            name="passwordInput"
+            id="password-input"
+            data-testid="password-input"
+            // value={ password }
+            // onChange={ console.log('passwordlChange') }
+          />
+        </label>
+
+        <button
+          type="submit"
+          name="submitBTN"
+          id="loggin-submit-btn"
+          data-testid="login-submit-btn"
+          // disabled={ true }
+          // onClick={ console.log('click') }
+        >
+          Enter
+        </button>
+
+      </form>
+    </div>
   );
 }
