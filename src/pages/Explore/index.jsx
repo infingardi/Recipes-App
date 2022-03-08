40f1d444dcb98b19ef9d8f8a01b740c41e8ccbb0
raphabarzotto <raphabarzotto@gmail.com>
2022-03-08 16:28:45 -0300

    feat: add explore pages

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function Explore() {
+  const { push } = useHistory();
   return (
     <div>
       <Header title="Explore" />
+      <main>
+        <button
+          type="button"
+          data-testid="explore-foods"
+          onClick={ () => push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
+        <button
+          type="button"
+          data-testid="explore-drinks"
+          onClick={ () => push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </main>
       <Footer />
     </div>
   );
--- src/pages/ExploreFoods/index.jsx
+++ src/pages/ExploreFoods/index.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 
 function ExploreFoods() {
+  const { push } = useHistory();
   return (
     <div>
       <Header title="Explore Foods" />
+      <main>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+          onClick={ () => push('/explore/foods/nationalities') }
+        >
+          By Nationality
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Surprise me!
+        </button>
+      </main>
       <Footer />
     </div>
   );
