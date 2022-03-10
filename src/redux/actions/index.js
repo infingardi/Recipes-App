90a8b975a2125881e1f9aac6e4d42a7aa83388e2
Tiago Meireles <tiagocf475@gmail.com>
2022-03-10 16:28:18 -0300

    feat: req 41 ao 46

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -6,17 +6,18 @@ export const SET_SELECTED_MEAL_INGREDIENT_TYPE = 'SET_SELECTED_MEAL_INGREDIENT';
 export const SET_SELECTED_DRINK_INGREDIENT_TYPE = 'SET_SELECTED_DRINK_INGREDIENT';
 export const SET_MEALS_CATEGORIES = 'SET_MEALS_CATEGORIES';
 export const SET_DRINKS_CATEGORIES = 'SET_DRINKS_CAGEGORIES';
+export const REMOVE_FAVORITE_TYPE = 'REMOVE_FAVORITE';
+export const ADD_PROGRESS_RECIPES_TYPE = 'ADD_PROGRESS_RECIPES';
 
 export const actionLogin = (newEmail) => ({
   type: ADD_LOGIN_TYPE,
   payload: newEmail,
 });
 
-export function actionAddFavorite() {
-  return {
-    type: ADD_FAVORITE_TYPE,
-  };
-}
+export const actionAddFavorite = (newFavorite) => ({
+  type: ADD_FAVORITE_TYPE,
+  payload: newFavorite,
+});
 
 export const actionAddDone = (doneRecipe) => ({
   type: ADD_DONE_TYPE,
@@ -46,4 +47,15 @@ export const setMealsCategory = (mealsCategoryResponse) => ({
 export const setDrinksCategory = (drinksCategoryResponse) => ({
   type: SET_DRINKS_CATEGORIES,
   payload: drinksCategoryResponse,
+});
+
+export const removeFavorites = (id) => ({
+  type: REMOVE_FAVORITE_TYPE,
+  payload: id,
+});
+
+export const setInProgressRecipes = (destiny, newProgress) => ({
+  type: ADD_PROGRESS_RECIPES_TYPE,
+  payload: newProgress,
+  destiny,
 });
